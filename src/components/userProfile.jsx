04187ea2cb8e6e719d1af4f8a290a774926aa303
Nikhilesh-Sanka <sankanikhilesh2006.svc@gmail.com>
2024-09-19 22:31:38 +0530

    facilited the viewing of the posts of the users in the user profile route

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Loading from "./Loading.jsx";
+import Post from "./Post.jsx";
 import { serverUrl } from "../config.js";
 import { TokenContext } from "../App.jsx";
 import UserProfileStyles from "../css-modules/userProfile.module.css";
 
 export default function UserProfile() {
   const [profile, setProfile] = useState(null);
+  const [posts, setPosts] = useState(null);
   const { token } = useContext(TokenContext);
   const navigate = useNavigate();
   const { state: userId } = useLocation();
@@ -26,29 +28,37 @@ export default function UserProfile() {
           navigate("/serverError");
         }
       })
-      .then((profile) => {
-        console.log(profile);
-        setProfile(profile);
+      .then((result) => {
+        setProfile(result.profile);
+        setPosts(result.posts);
       });
   }, []);
 
   return (
-    <>
+    <div className={UserProfileStyles["container"]}>
       {profile ? (
-        <div className={UserProfileStyles["user-profile"]}>
-          <h2>{profile.username}</h2>
-          <img src={profile.profile.image} />
-          <p>
-            <span>First Name:</span> {profile.firstName}
-          </p>
-          <p>
-            <span>Last Name:</span> {profile.lastName}
-          </p>
-          <p>{profile.profile.bio}</p>
-        </div>
+        <>
+          <div className={UserProfileStyles["user-profile"]}>
+            <h2>{profile.username}</h2>
+            <img src={profile.profile.image} />
+            <p>
+              <span>First Name:</span> {profile.firstName}
+            </p>
+            <p>
+              <span>Last Name:</span> {profile.lastName}
+            </p>
+            <p>{profile.profile.bio}</p>
+          </div>
+          <div className={UserProfileStyles["user-posts"]}>
+            <h2>{profile.username} posts</h2>
+            {posts.map((post) => {
+              return <Post post={post} key={post.id} />;
+            })}
+          </div>
+        </>
       ) : (
         <Loading />
       )}
-    </>
+    </div>
   );
 }
